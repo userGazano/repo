0a370d9e611dfb7bfaad15e7712ccab9db9729bd
userGazano <sixsevengazan@proton.me>
2026-08-15 00:59:12 +0400

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,11 @@ ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', '@Withoutx4')
 TELEGRAM_API_ID = int(os.getenv('TELEGRAM_API_ID', '0'))
 TELEGRAM_API_HASH = os.getenv('TELEGRAM_API_HASH')
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///shop.db')
+# ✅ SQLite с абсолютным путём
+DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:////tmp/shop.db')
 
 SHOP_NAME = os.getenv('SHOP_NAME', 'PHYSICAL SHOP')
 CURRENCY = os.getenv('CURRENCY', '⭐️')
 
-SESSIONS_DIR = './sessions'
-LOGS_DIR = './logs'
+SESSIONS_DIR = '/tmp/sessions'
+LOGS_DIR = '/tmp/logs'
