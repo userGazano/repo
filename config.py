8146cb0441452c625fc2e01a68f15caa2b422c60
userGazano <sixsevengazan@proton.me>
2026-08-14 13:03:53 +0400

    Add default values for environment variables
    Set default values for ADMIN_ID and TELEGRAM_API_ID.

--- config.py
+++ config.py
@@ -3,18 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# ========== BOT ==========
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID = int(os.getenv('ADMIN_ID'))
+ADMIN_ID = int(os.getenv('ADMIN_ID', '0'))
 ADMIN_USERNAME = os.getenv('ADMIN_USERNAME', '@Withoutx4')
-
-# ========== TELEGRAM API ==========
-TELEGRAM_API_ID = int(os.getenv('TELEGRAM_API_ID'))
+TELEGRAM_API_ID = int(os.getenv('TELEGRAM_API_ID', '0'))
 TELEGRAM_API_HASH = os.getenv('TELEGRAM_API_HASH')
-
-# ========== DATABASE ==========
 DATABASE_URL = os.getenv('DATABASE_URL')
-
-# ========== SHOP ==========
 SHOP_NAME = os.getenv('SHOP_NAME', 'PHYSICAL SHOP')
 CURRENCY = os.getenv('CURRENCY', '₽')
